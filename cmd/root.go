3badf6f2128dfcaf8d71c1644a11a242fd29109b
daichi2mori <daichi2mori@gmail.com>
2024-10-13 23:50:00 +0900

    feat: add version option

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,23 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+type Config struct {
+	version string
+}
 
+var config Config
+var cfgFile string = "config.yml"
+var versionFlag bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,6 +32,18 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if versionFlag {
+			// Viperからバージョン情報を取得
+			version := viper.GetString("version")
+			if version == "" {
+				fmt.Println("バージョン情報が見つかりません")
+			} else {
+				fmt.Println("バージョン: ", version)
+			}
+			os.Exit(0)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,6 +56,7 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -46,6 +66,18 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// --version, -v フラグを設定
+	rootCmd.Flags().BoolVarP(&versionFlag, "version", "v", false, "バージョン情報を表示します")
 }
 
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
 
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	viper.ConfigFileUsed()
+}
